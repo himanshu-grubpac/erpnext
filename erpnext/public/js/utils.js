819c870aa01eb5c9b78d577f5cdbe78dd28b0543
Nishka Gosalia <nishkagosalia@Nishkas-MacBook-Air.local>
2026-01-12 17:06:49 +0530

    feat: Adding Item name in update item dialog box
    (cherry picked from commit e6133ad6d467885d246030bb2b144927a9357d6a)
    
    # Conflicts:
    #	erpnext/public/js/utils.js

--- erpnext/public/js/utils.js
+++ erpnext/public/js/utils.js
@@ -730,6 +730,16 @@ erpnext.utils.update_child_items = function (opts) {
 			},
 		},
 		{
+<<<<<<< HEAD
+=======
+			fieldtype: "Data",
+			fieldname: "item_name",
+			label: __("Item Name"),
+			read_only: 1,
+			in_list_view: 1,
+		},
+		{
+>>>>>>> e6133ad6d4 (feat: Adding Item name in update item dialog box)
 			fieldtype: "Link",
 			fieldname: "uom",
 			options: "UOM",
@@ -779,14 +789,14 @@ erpnext.utils.update_child_items = function (opts) {
 	];
 
 	if (frm.doc.doctype == "Sales Order" || frm.doc.doctype == "Purchase Order") {
-		fields.splice(2, 0, {
+		fields.splice(3, 0, {
 			fieldtype: "Date",
 			fieldname: frm.doc.doctype == "Sales Order" ? "delivery_date" : "schedule_date",
 			in_list_view: 1,
 			label: frm.doc.doctype == "Sales Order" ? __("Delivery Date") : __("Reqd by date"),
 			reqd: 1,
 		});
-		fields.splice(3, 0, {
+		fields.splice(4, 0, {
 			fieldtype: "Float",
 			fieldname: "conversion_factor",
 			label: __("Conversion Factor"),
