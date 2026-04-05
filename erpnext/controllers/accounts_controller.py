f08704fcfc0170053a6e64431405d20f21f28f39
ervishnucs <ervishnucs369@gmail.com>
2026-04-05 11:16:37 +0530

    fix: resolve item tax template from item group in update items
    (cherry picked from commit ad22256b2d8c35462ac46fdbcbb698ee4a522ba1)
    
    # Conflicts:
    #	erpnext/stock/get_item_details.py

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -76,6 +76,7 @@ from erpnext.stock.get_item_details import (
 =======
 	ItemDetailsCtx,
 	_get_item_tax_template,
+	_get_item_tax_template_from_item_group,
 	get_conversion_factor,
 	get_item_details,
 	get_item_tax_map,
@@ -3668,11 +3669,7 @@ def set_child_tax_template_and_map(item, child_item, parent_doc):
 	item_tax_template = _get_item_tax_template(ctx, item.taxes)
 
 	if not item_tax_template:
-		item_group = item.item_group
-		while item_group and not item_tax_template:
-			item_group_doc = frappe.get_cached_doc("Item Group", item_group)
-			item_tax_template = _get_item_tax_template(ctx, item_group_doc.taxes)
-			item_group = item_group_doc.parent_item_group
+		item_tax_template = _get_item_tax_template_from_item_group(ctx, item.item_group)
 
 	child_item.item_tax_template = item_tax_template
 >>>>>>> 97e7916b66 (fix: resolve item tax template from item group in update items)
