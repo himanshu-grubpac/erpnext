1c44c60dbdba6cd4a8d5c03fe231e45af0b1acc1
Rohit Waghchaure <rohitw1991@gmail.com>
2026-05-13 13:16:00 +0530

    fix: posting date and time
    (cherry picked from commit fb6c05f186c91b117eb9e783e8df5607dc4f09b3)

--- erpnext/stock/serial_batch_bundle.py
+++ erpnext/stock/serial_batch_bundle.py
@@ -13,6 +13,7 @@ from erpnext.stock.deprecated_serial_batch import (
 	DeprecatedBatchNoValuation,
 	DeprecatedSerialNoValuation,
 )
+from erpnext.stock.utils import get_combine_datetime
 from erpnext.stock.valuation import round_off_if_near_zero
 
 CONSUMED_SERIAL_NO_STOCK_ENTRY_PURPOSES = (
@@ -1069,6 +1070,10 @@ class SerialBatchCreation:
 		self.__dict__.update(item_details)
 
 	def set_other_details(self):
+		if not self.get("posting_datetime"):
+			if self.get("posting_date") and self.get("posting_time"):
+				self.posting_datetime = get_combine_datetime(self.posting_date, self.posting_time)
+
 		if not self.get("posting_datetime"):
 			self.posting_datetime = now()
 			self.__dict__["posting_datetime"] = self.posting_datetime
