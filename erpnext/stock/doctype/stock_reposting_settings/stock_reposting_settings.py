6e6ef83d60d3f42b32b2d578889e8bd5f0f2e62b
Rohit Waghchaure <rohitw1991@gmail.com>
2026-05-15 13:49:41 +0530

    fix: incoming rate for legacy serial no
    (cherry picked from commit 2773b7c0022bbad0e872d05289fdfedeac0a79f7)
    
    # Conflicts:
    #	erpnext/stock/deprecated_serial_batch.py
    #	erpnext/stock/doctype/stock_reposting_settings/stock_reposting_settings.json
    #	erpnext/stock/doctype/stock_reposting_settings/stock_reposting_settings.py

--- erpnext/stock/doctype/stock_reposting_settings/stock_reposting_settings.py
+++ erpnext/stock/doctype/stock_reposting_settings/stock_reposting_settings.py
@@ -16,6 +16,12 @@ class StockRepostingSettings(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+<<<<<<< HEAD
+=======
+		do_not_fetch_incoming_rate_from_serial_no: DF.Check
+		enable_parallel_reposting: DF.Check
+		enable_separate_reposting_for_gl: DF.Check
+>>>>>>> 2773b7c002 (fix: incoming rate for legacy serial no)
 		end_time: DF.Time | None
 		item_based_reposting: DF.Check
 		limit_reposting_timeslot: DF.Check
